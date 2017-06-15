02a07b81342e49140e8575a218683ba6fc16b7fa
John <supertonesfan@hotmail.com>
2017-06-15 08:14:26 -0500

    Search returns API response

--- app/components/search-bar.js
+++ app/components/search-bar.js
@@ -1,16 +1,21 @@
 import Ember from 'ember';
+import config from '../config/environment';
 
 export default Ember.Component.extend({
   query: '',
 
-  actualQuery: Ember.computed('query', function() { 
-    console.log('actualquery function is called: ', this.get('query'));
-  }),
-
-  queryChanged: Ember.observer('query', function() { 
-  	var query = this.get('query');
-  	if(query.length > 3){
-  		console.log('do some action')
-  	}
+  queryChanged: Ember.observer('query', function() {
+    var query = this.get('query');
+    if(query.length > 3){
+      let request = {
+        client_id: config.apiKey,
+        limit: "10",
+        offset: "0",
+        query: query
+      };
+      Ember.$.get("https://api.twitch.tv/kraken/search/streams", request).then((response) => {
+        console.log(response)
+      });
+    }
   })
 });
--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,7 @@ module.exports = function(environment) {
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
+    apiKey: 'yr4l41x0wdtfarlstr6drfap13zeo2',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
